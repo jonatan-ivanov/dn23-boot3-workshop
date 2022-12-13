2890d75b0c3b66506a0a8893430b33b0f3f623d0
Madhura Bhave <bhavem@vmware.com>
2022-12-13 15:20:36 -0800

    Add bean definition manually

--- src/main/java/example/helloworld/HelloWorldApplication.java
+++ src/main/java/example/helloworld/HelloWorldApplication.java
@@ -1,15 +1,33 @@
 package example.helloworld;
 
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.support.GenericApplicationContext;
 
 @EnableAutoConfiguration
 @ComponentScan
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloWorldApplication.class, args);
+		SpringApplication builder = new SpringApplicationBuilder(HelloWorldApplication.class)
+				.initializers(new HelloInitializer()).build();
+		builder.run(args);
+	}
+
+	static class HelloInitializer implements ApplicationContextInitializer<GenericApplicationContext> {
+
+		@Override
+		public void initialize(GenericApplicationContext applicationContext) {
+			BeanDefinitionRegistry registry = applicationContext;
+			RootBeanDefinition beanDefinition = new RootBeanDefinition(HelloNameProvider.class);
+			registry.registerBeanDefinition("helloNameProvider", beanDefinition);
+		}
+
 	}
 
 }
