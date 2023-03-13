dfa3e2b06f69a594fb1876fd8346bb2a4170b7d9
Phillip Webb <pwebb@vmware.com>
2023-03-13 11:38:27 -0700

    Add setup instructions
    Co-authored-by: Jonatan Ivanov <jonatan.ivanov@gmail.com>

---
+++ src/main/java/ConfigServerClient_Java8_SB_260/ConfigServerClient_Java8_SB_260/Application.java
@@ -0,0 +1,13 @@
+package ConfigServerClient_Java8_SB_260.ConfigServerClient_Java8_SB_260;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
