4ccc87cea039fadb2176866f732dc311a60b8a8c
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2023-03-23 16:21:56 -0700

    Disable Actuator Observations

--- dog-service/src/main/java/com/example/dogservice/actuator/ActuatorConfiguration.java
+++ dog-service/src/main/java/com/example/dogservice/actuator/ActuatorConfiguration.java
@@ -2,13 +2,25 @@ package com.example.dogservice.actuator;
 
 import io.micrometer.common.KeyValue;
 import io.micrometer.observation.ObservationFilter;
+import io.micrometer.observation.ObservationPredicate;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.observation.ServerRequestObservationContext;
 
 @Configuration(proxyBeanMethods = false)
 public class ActuatorConfiguration {
+
+	@Bean
+	ObservationPredicate noActuatorObservations() {
+		return (name, context) -> {
+			if (name.equals("http.server.requests") && context instanceof ServerRequestObservationContext serverContext) {
+				return !serverContext.getCarrier().getRequestURI().startsWith("/actuator");
+			}
+			return true;
+		};
+	}
 
 	@Bean
 	ObservationFilter orgFilter(@Value("${spring.application.org}") String org) {
