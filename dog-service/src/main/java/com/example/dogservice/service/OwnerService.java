c12739690046e82e402d173770e79045ec9c2c14
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2023-03-25 01:03:23 -0700

    Add custom Observation

--- dog-service/src/main/java/com/example/dogservice/service/OwnerService.java
+++ dog-service/src/main/java/com/example/dogservice/service/OwnerService.java
@@ -8,6 +8,8 @@ import com.example.dogservice.domain.Dog;
 import com.example.dogservice.domain.DogRepository;
 import com.example.dogservice.domain.Owner;
 import com.example.dogservice.domain.OwnerRepository;
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationRegistry;
 
 /**
  * Dog owner service.
@@ -19,14 +21,25 @@ public class OwnerService {
 
 	private final DogRepository dogRepository;
 
-	public OwnerService(OwnerRepository ownerRepository, DogRepository dogRepository) {
+	private final ObservationRegistry observationRegistry;
+
+	public OwnerService(OwnerRepository ownerRepository, DogRepository dogRepository,
+			ObservationRegistry observationRegistry) {
 		this.ownerRepository = ownerRepository;
 		this.dogRepository = dogRepository;
+		this.observationRegistry = observationRegistry;
 	}
 
 	public List<String> getOwnedDogNames(String ownerName) {
+		return Observation.createNotStarted("getDogs", observationRegistry)
+				.contextualName("gettingOwnedDogs")
+				.highCardinalityKeyValue("ownerName", ownerName)
+				.observe(() -> getOwnedDogNamesInternal(ownerName));
+	}
+
+	private List<String> getOwnedDogNamesInternal(String ownerName) {
 		Owner owner = this.ownerRepository.findByNameIgnoringCase(ownerName);
-		if(owner == null) {
+		if (owner == null) {
 			throw new NoSuchDogOwnerException(ownerName);
 		}
 		return this.dogRepository.findByOwner(owner).stream().map(Dog::getName).toList();
